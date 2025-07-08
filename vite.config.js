18b3c560b35bb02dffe55f20d8d108794abf7318
David Benner <119465113+davebenner14@users.noreply.github.com>
2025-07-08 14:56:45 -0400

    Add Template section

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist" // ensures consistent output location
+  },
+  server: {
+    historyApiFallback: true // enables fallback routing in dev mode
+  }
+});
